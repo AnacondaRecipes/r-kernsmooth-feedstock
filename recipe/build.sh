945568ceea1a69a90a60f228037c531fc485cc6e
Kai Tietz <ktietz@anaconda.com>
2019-05-10 14:18:05 +0200

    Corrected to use .dylib instead of .so

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,5 +4,5 @@ $R CMD INSTALL --build .
 # This rpath interferes with my attempt at allowing symbolic-link based interposition of Rblas.
 if [[ ${target_platform} == osx-64 ]]; then
   find ${PREFIX}/lib/R/library
-  ${INSTALL_NAME_TOOL} -delete_rpath ${PREFIX}/lib ${PREFIX}/lib/R/library/KernSmooth/libs/KernSmooth.so
+  ${INSTALL_NAME_TOOL} -delete_rpath ${PREFIX}/lib ${PREFIX}/lib/R/library/KernSmooth/libs/KernSmooth.dylib
 fi
